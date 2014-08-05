79d1271f5174ad415d033e30ce9d55c14067ed3f
ubuntu <ubuntu@ubuntu-virtual-machine.(none)>
2014-08-05 06:00:03 -0700

    strength reminders

--- biofeedback-master/src/nz/ac/waikato/isdb/StrengthReminderActivity.java
+++ biofeedback-master/src/nz/ac/waikato/isdb/StrengthReminderActivity.java
@@ -24,6 +24,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StrengthReminderActivity extends Activity {
 
@@ -35,19 +36,23 @@ public class StrengthReminderActivity extends Activity {
 	HashMap< Integer, Integer> sortedLike = new HashMap<Integer, Integer>();
 
 	SharedPreferences pref,pref1;
-	TextView t1;
-	Button manual,random;
+	TextView t1,t2,t3;
+	Button random;
 	String[] reminderText;
 	int visibility;
 	Set<String> set;
+	View v1,v2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_strength_reminders);
-		t1 =(TextView)findViewById(R.id.reminderText);
+		t1 =(TextView)findViewById(R.id.reminderText1);
+		t2 =(TextView)findViewById(R.id.reminderText2);
+		t3 =(TextView)findViewById(R.id.reminderText3);
+		v1 = findViewById(R.id.view1);
+		v2 = findViewById(R.id.view2);
 		reminderText = getResources().getStringArray(R.array.reminders);
-		manual = (Button)findViewById(R.id.buttonManual);
 		random = (Button)findViewById(R.id.buttonRandom);
 		pref = getSharedPreferences("physical", MODE_PRIVATE);
 		int j=0;
@@ -84,62 +89,33 @@ public class StrengthReminderActivity extends Activity {
 		}
 		sortedLike = sortByValues(like);
 		sortedLike = getHighRatedValues(sortedLike);
-		pref = getSharedPreferences("mode", MODE_PRIVATE);
-		visibility = pref.getInt("mode", 0);
-
-		if(visibility == 0)
-		{
-			manual.setVisibility(View.VISIBLE);
-			random.setVisibility(View.GONE);
-			setRandom();
-		}
+		pref1 = getSharedPreferences("randomstrings", MODE_PRIVATE);
+		if((pref1.getInt("key1", -1) != -1) && sortedLike.size() > 3)
+			setDefault();
 		else
-		{
-			manual.setVisibility(View.GONE);
-			random.setVisibility(View.VISIBLE);
-			setManual();
+			setRandom();
 
-		}
-		manual.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				random.setVisibility(View.VISIBLE);
-				manual.setVisibility(View.GONE);
-				pref.edit().putInt("mode", 1).commit();
-				Intent intent = new Intent(getApplicationContext(), StrengthReminders.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				startActivity(intent);
-			}
-		});
 		random.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				manual.setVisibility(View.VISIBLE);
-				random.setVisibility(View.GONE);
-				pref.edit().putInt("mode", 0).commit();
-				setRandom();
+				if(sortedLike.size() == 0)
+					Toast.makeText(getApplicationContext(), "Answer Questions in Self Assement to get Strength Reminders", Toast.LENGTH_LONG).show();
+				else if(sortedLike.size() > 3)
+					setRandom();
+				else if(sortedLike.size()<=3)
+					Toast.makeText(getApplicationContext(), "Answer more than 3 Questions in Self Assement to randomize Strength Reminders", Toast.LENGTH_LONG).show();
 			}
 		});
 
 	}
 
-	public void setManual()
+	public void setDefault()
 	{
-		t1.setText("");
-		pref1 = getSharedPreferences("reminderarray", MODE_PRIVATE);
-		set = pref1.getStringSet("reminderarray", null);
-		if(set == null)
-			return;
-		Iterator iterator2 = set.iterator();
-		int i=1;
-		while(iterator2.hasNext()) {
-			String me2 = (String)iterator2.next();
-			t1.setText(t1.getText()+ " \n"+i+": "+ me2);
-			i++;
-		}
-
+		t1.setText(reminderText[pref1.getInt("key1", -1)]);
+		t2.setText(reminderText[pref1.getInt("key2", -1)]);
+		t3.setText(reminderText[pref1.getInt("key3", -1)]);
 	}
 
 	public void setRandom()
@@ -149,31 +125,45 @@ public class StrengthReminderActivity extends Activity {
 		ArrayList numbers = new ArrayList(); 
 		int k =1;
 		t1.setText("");
+		t3.setText("");
+		t2.setText("");
+		t2.setVisibility(View.VISIBLE);
+		t3.setVisibility(View.VISIBLE);
+		v1.setVisibility(View.VISIBLE);
+		v2.setVisibility(View.VISIBLE);
 		int       randomKey =-1;
 		Set set = sortedLike.entrySet();
 		Iterator iterator2 = set.iterator();
-		while(iterator2.hasNext()) {
-			if(k<=3)
-			{
-				Map.Entry me2 = (Map.Entry)iterator2.next();
-				if((Integer)me2.getValue()==-1)
-				{
-					if(k==1)
-					{
-						t1.setText("Answere Questons in Self Assement to get Strength Reminders");
-						break;
-					}
-				}
-				else
-				{
-					t1.setText(t1.getText()+ " \n"+k+": "+reminderText[(Integer) me2.getKey()]);
+		if(sortedLike.size() == 0)
+		{
+			t1.setText("Answer Questions in Self Assement to get Strength Reminders");
+			t2.setVisibility(View.GONE);
+			t3.setVisibility(View.GONE);
+			v1.setVisibility(View.GONE);
+			v2.setVisibility(View.GONE);
+			Log.d("ashwini", "con 1 ");
 
-					k=k+1;
+		}
+
+		else if(sortedLike.size() > 3)
+		{
+			Log.d("ashwini", "con 2");
+			while(iterator2.hasNext()) {
+				if(k<=3)
+				{
+					Map.Entry me2 = (Map.Entry)iterator2.next();
+
+					//t1.setText(t1.getText()+ " \n"+k+": "+reminderText[(Integer) me2.getKey()]);
+
+
 
 					List<Integer> keys      = new ArrayList<Integer>(sortedLike.keySet());
 					if(k==1)
 					{
 						randomKey = keys.get( random.nextInt(keys.size()) );
+						t1.setText(k+": "+reminderText[randomKey]);
+						pref1.edit().putInt("key1", randomKey).apply();
+						numbers.add(randomKey);
 
 					}
 					else{
@@ -188,18 +178,70 @@ public class StrengthReminderActivity extends Activity {
 							}
 						}
 					}
+
+					if(k==2)
+					{
+						t2.setText(k+": "+reminderText[randomKey]);
+						pref1.edit().putInt("key2", randomKey).apply();
+					}
+					if(k==3)
+					{
+						t3.setText(k+": "+reminderText[randomKey]);
+
+						pref1.edit().putInt("key3", randomKey).apply();
+					}
 					int       value     = sortedLike.get(randomKey);
 					Log.d("ashwiniii key",randomKey + ": ");
+					//t1.setText(t1.getText()+ " \n"+k+": "+reminderText[randomKey]);
+					k=k+1;
 					Log.d("ashwiniii value"," "+ value);
 
-				}
-			}
-			else{
-				Log.d("ashwini key","break");
-				break;
-			}
 
+				}
+				else{
+					Log.d("ashwini key","break");
+					break;
+				}
+
+			}
 		}
+		else if(sortedLike.size() <= 3)
+		{
+
+			Log.d("ashwini", "con 3 ");
+			while(iterator2.hasNext())
+			{
+				Map.Entry me2 = (Map.Entry)iterator2.next();
+				if (k == 1)
+				{
+					t1.setText(k+": "+reminderText[(Integer) me2.getKey()]);
+					t2.setVisibility(View.GONE);
+					t3.setVisibility(View.GONE);
+					v1.setVisibility(View.GONE);
+					v2.setVisibility(View.GONE);
+				}
+				else if (k == 2)
+				{
+
+					t2.setVisibility(View.VISIBLE);
+					t3.setVisibility(View.GONE);
+					v1.setVisibility(View.VISIBLE);
+					v2.setVisibility(View.GONE);
+					t2.setText(k+": "+reminderText[(Integer) me2.getKey()]);
+				}
+				else if (k == 3)
+				{
+					t2.setVisibility(View.VISIBLE);
+					t3.setVisibility(View.VISIBLE);
+					v1.setVisibility(View.VISIBLE);
+					v2.setVisibility(View.VISIBLE);
+					t3.setText(k+": "+reminderText[(Integer) me2.getKey()]);
+				}
+				k++;
+			}
+		}
+
+
 	}
 	private HashMap getHighRatedValues(HashMap map)
 	{
@@ -220,19 +262,7 @@ public class StrengthReminderActivity extends Activity {
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		if(visibility == 0)
-		{
-			manual.setVisibility(View.VISIBLE);
-			random.setVisibility(View.GONE);
-			setRandom();
-		}
-		else
-		{
-			manual.setVisibility(View.GONE);
-			random.setVisibility(View.VISIBLE);
-			setManual();
 
-		}
 
 		// testing values
 		/*Log.d("ashwini","Before Sorting:");
